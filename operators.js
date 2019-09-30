924c06cce4697de4a0586f2b64fd5d53fc71bbeb
shehzad <slak786@gmail.com>
2019-09-30 09:23:12 -0400

    operators

---
+++ operators.js
@@ -0,0 +1,13 @@
+var myAge = 31;
+
+if(myAge <= 3){
+    console.log("You are a baby");
+}else if(myAge <=18){
+    console.log("You are a child");
+}else if(myAge > 18){
+    console.log("You are an adult");
+}
+
+var votingAge = 21;
+
+if(votingAge == 21){console.log("You can vote")};
