06e47816cedc88933e8200ba3fa844ddd08d3fb3
shehzad <slak786@gmail.com>
2019-09-30 13:42:30 -0400

    switch statements

---
+++ switch.js
@@ -0,0 +1,13 @@
+var animal = "Horse";
+switch (animal){
+    case "Cow" :
+    case "Cat" :
+    case "Horse" :
+    console.log("This is a great animal!");
+    break;
+    default:
+    console.log("Not an animal!");
+
+
+}
+
