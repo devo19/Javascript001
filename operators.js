90cef4b7695d70978eefb08ffd89b6d39fabba3f
shehzad <slak786@gmail.com>
2019-09-30 13:27:09 -0400

    added Ternary operators

--- operators.js
+++ operators.js
@@ -11,3 +11,14 @@ if(myAge <= 3){
 var votingAge = 21;
 
 if(votingAge == 21){console.log("You can vote")};
+
+// Ternary operators
+var yourAge = 10;
+console.log((yourAge>=18) ? "You can vote" : "You are too young"); // condensed way to write if/else
+
+// Ternary operator inside another Ternary operator
+var firstCheck = false;
+var secondCheck = false;
+access = firstCheck ? "access denied" : secondCheck ? "access denied" : "access granted";
+console.log(access);
+
