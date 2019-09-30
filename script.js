47f69d21a70297f49b77caa9e0a9059c32f7991d
shehzad <slak786@gmail.com>
2019-09-30 01:27:34 -0400

    changes

---
+++ script.js
@@ -0,0 +1,32 @@
+
+function square(num){
+    return num * num;
+}
+console.log(square(2));
+
+
+var myVar = "Hello";
+function someFunc(){
+    var myVar = "Hi";
+    console.log(myVar)
+}
+someFunc();
+console.log(myVar);
+
+
+var num = 100;
+if(num < 10 || num < 50){
+    console.log('great');
+}else{
+    console.log('not great');
+}
+
+console.log(3 == "3");
+console.log(3 === "3");
+
+console.log(true == '1');
+console.log(true === '1');
+
+console.log('hello from JS');
+    
+
